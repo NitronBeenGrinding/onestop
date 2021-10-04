fa509aa70aa2e7a8c3895dd4c33bed715edbc689
Abinav Sadhneni <asadhneni@gmail.com>
2021-10-04 00:50:33 -0400

    Added sports to browse screen

--- screens/browseScreen.js
+++ screens/browseScreen.js
@@ -1,27 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
+import { Card, ListItem, Icon } from 'react-native-elements'
+
+const list = [
+  {
+    name: 'Basketball',
+    icon: 'basketball-outline'
+  },
+  {
+    name: 'Football',
+    icon: 'american-football-outline'
+  }
+]
 
 export default function BrowseScreen() {
   return (
-    <View style={styles.container}>
-      <Text>OneStop Ticket Exchange.</Text>
-      <Button
-        onPress={() => {
-          alert('test!');
-        }}
-        title="Browse Screen!"
-      />
-      <StatusBar style="auto" />
+    <View>
+      {
+        list.map((l, i) => (
+          <ListItem key={i} bottomDivider>
+          <Icon name={l.icon} type="ionicon"></Icon>
+            <ListItem.Content>
+              <ListItem.Title>{l.name}</ListItem.Title>
+            </ListItem.Content>
+            <ListItem.Chevron />
+          </ListItem>
+        ))
+      }
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
